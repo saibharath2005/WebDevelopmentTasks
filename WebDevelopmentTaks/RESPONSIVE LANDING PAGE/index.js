a0e018994efecf7e1e95df3b5e6821dc9582a4fa
saibharath2005 <151985944+saibharath2005@users.noreply.github.com>
2024-11-23 07:34:03 +0530

    Add files via upload

---
+++ WebDevelopmentTaks/RESPONSIVE LANDING PAGE/index.js
@@ -0,0 +1,17 @@
+// Add 'scrolled' class to navbar on scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }// Add 'scrolled' class to navbar on scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  });
